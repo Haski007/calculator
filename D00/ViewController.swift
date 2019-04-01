c4702a5794ec262194cb8b92c99274a2b70178d4
Pikaliuk DEMIAN <pdemian@e1r5p9.unit.ua>
2019-04-01 16:28:14 +0300

    Initial Commit

---
+++ D00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  D00
+//
+//  Created by Pikaliuk DEMIAN on 4/1/19.
+//  Copyright © 2019 Pikaliuk DEMIAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
